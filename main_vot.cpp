b1d718cd64681fb66642062bb153550aff325c14
Tomas Vojir <vojirtom@cmp.felk.cvut.cz>
2016-02-12 15:03:40 +0100

    change VOT interface to the new VOT2014/VOT2015 methodology (backward compatible with VOT2013)

--- main_vot.cpp
+++ main_vot.cpp
@@ -3,33 +3,34 @@
 #include "colotracker.h"
 #include "region.h"
 #include <string>
+#include <limits>
 
-#include "cpp_vot_io.hpp"
+#include "vot.hpp"
 
 using namespace cv;
 
 int main(int, char **) 
 {
     ColorTracker tracker;
-    cv::Mat img;
+    cv::Mat image;
 
     //load region, images and prepare for output
-    CPP_VOT_IO vot_io("region.txt", "images.txt", "output.txt");
+    VOT vot_io("region.txt", "images.txt", "output.txt");
     
     //img = firts frame, initPos = initial position in the first frame
-    cv::Rect initPos = vot_io.getInitRectangle();
-    vot_io.outputBoundingBox(initPos);
-    vot_io.getNextImage(img);
+    cv::Rect init_rect = vot_io.getInitRectangle();
+    vot_io.outputBoundingBox(init_rect);
+    vot_io.getNextImage(image);
 
     //tracker initialization
-    tracker.init(img, initPos.x, initPos.y, initPos.x + initPos.width, initPos.y + initPos.height);
+    tracker.init(image, init_rect.x, init_rect.y, init_rect.x + init_rect.width, init_rect.y + init_rect.height);
 
     //track   
     double average_speed_ms = 0.0;
     double num_frames = 0.0;
-    while (vot_io.getNextImage(img) == 1){
+    while (vot_io.getNextImage(image) == 1){
         double time_profile_counter = cv::getCPUTickCount();
-        BBox * bb = tracker.track(img);
+        BBox * bb = tracker.track(image);
         time_profile_counter = cv::getCPUTickCount() - time_profile_counter;
         average_speed_ms += time_profile_counter/((double)cvGetTickFrequency()*1000);
         num_frames += 1.0;
@@ -37,8 +38,11 @@ int main(int, char **)
         if (bb != NULL){
             vot_io.outputBoundingBox(cv::Rect(bb->x, bb->y, bb->width, bb->height));
             delete bb;
+        } else {
+            vot_io.outputBoundingBox(cv::Rect(std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN()));
         }
     }
+
     std::cout << "Average speed " <<  average_speed_ms/num_frames << "ms. (" << 1000.0/(average_speed_ms/num_frames) << "fps)" << std::endl;
 
     return 0;
