ac87525d9d62482bb127c4702737c3f87dc928f5
Tomas Vojir <vojirtom@cmp.felk.cvut.cz>
2016-02-12 14:55:35 +0100

    added output of the average processing speed per frame

--- main_vot.cpp
+++ main_vot.cpp
@@ -25,12 +25,21 @@ int main(int, char **)
     tracker.init(img, initPos.x, initPos.y, initPos.x + initPos.width, initPos.y + initPos.height);
 
     //track   
+    double average_speed_ms = 0.0;
+    double num_frames = 0.0;
     while (vot_io.getNextImage(img) == 1){
+        double time_profile_counter = cv::getCPUTickCount();
         BBox * bb = tracker.track(img);
+        time_profile_counter = cv::getCPUTickCount() - time_profile_counter;
+        average_speed_ms += time_profile_counter/((double)cvGetTickFrequency()*1000);
+        num_frames += 1.0;
+
         if (bb != NULL){
             vot_io.outputBoundingBox(cv::Rect(bb->x, bb->y, bb->width, bb->height));
             delete bb;
         }
     }
+    std::cout << "Average speed " <<  average_speed_ms/num_frames << "ms. (" << 1000.0/(average_speed_ms/num_frames) << "fps)" << std::endl;
+
     return 0;
 }
