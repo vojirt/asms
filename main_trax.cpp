a62825817bf042b4c6602b321bda9d7004e45377
Luka Čehovin <luka.cehovin@gmail.com>
2016-09-01 15:33:46 +0100

    Adding TraX protocol support, updating OpenCV compatibility for 3.x.

---
+++ main_trax.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "colotracker.h"
+#include "region.h"
+#include <string>
+#include <limits>
+#include <trax/opencv.hpp>
+
+using namespace cv;
+
+int main(int, char **) 
+{
+    trax::Image img;
+    trax::Region reg;
+
+    cv::Ptr<ColorTracker> tracker;
+    cv::Mat image;
+	cv::Rect rectangle;
+
+    trax::Server handle(trax::Configuration(TRAX_IMAGE_PATH | TRAX_IMAGE_MEMORY | TRAX_IMAGE_BUFFER, TRAX_REGION_RECTANGLE), trax_no_log);
+
+    while(true)
+    {
+
+        trax::Properties prop;
+        trax::Region status;
+
+        int tr = handle.wait(img, reg, prop);
+
+        if (tr == TRAX_INITIALIZE) {
+
+            rectangle = trax::region_to_rect(reg);
+            image = trax::image_to_mat(img);
+
+            tracker = new ColorTracker();
+
+			tracker->init(image, rectangle.x, rectangle.y, rectangle.x + rectangle.width, rectangle.y + rectangle.height);
+
+            status = trax::rect_to_region(rectangle);
+
+        } else if (tr == TRAX_FRAME) {
+
+            image = trax::image_to_mat(img);
+			BBox* bb = tracker->track(image);
+
+            if (bb != NULL) {
+			    rectangle = cv::Rect(bb->x, bb->y, bb->width, bb->height);
+                status = trax::rect_to_region(rectangle);
+            } else {
+                status = trax::Region::create_special(0);
+            }
+
+        } else {
+            break;
+        }
+
+        handle.reply(status, trax::Properties());
+
+    }
+
+    return EXIT_SUCCESS;
+
+}
